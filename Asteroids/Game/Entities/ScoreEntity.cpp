9a5038d6d2e0396a997959200fb5f457c6495269
Daniel Ricci <thedanny09@icloud.com>
2021-02-06 01:47:08 -0500

    Better positioning and sizing of score

--- Asteroids/Game/Entities/ScoreEntity.cpp
+++ Asteroids/Game/Entities/ScoreEntity.cpp
@@ -4,8 +4,9 @@
 ScoreEntity::ScoreEntity() {
     TextComponent* textComponent = new TextComponent("Hyperspace.ttf");
     textComponent->setText("12345");
-    textComponent->setSize(24);
+    textComponent->setSize(32);
     this->addComponent(textComponent);
+    this->setPosition(Eigen::Vector2f(150, 50));
 }
 
 void ScoreEntity::render(SDL_Renderer& renderer) {
