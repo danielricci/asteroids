f7bbe65df67feb585e55fc0fb6a958517167178f
Daniel Ricci <thedanny09@icloud.com>
2021-02-07 01:34:00 -0500

    Score now updates dynamically based on asteroid hit

--- Asteroids/Game/Entities/ScoreEntity.cpp
+++ Asteroids/Game/Entities/ScoreEntity.cpp
@@ -8,29 +8,41 @@ ScoreEntity::ScoreEntity() {
     TextComponent* textComponent = new TextComponent("Hyperspace.ttf");
     textComponent->setSize(32);
     this->addComponent(textComponent);
+
     this->setPosition(Eigen::Vector2f(150, 50));
-    this->addScore(12);
 }
 
 void ScoreEntity::addScore(int score) {
     this->score += score;
-    getComponent<TextComponent>()->setText(toString());
+    isDirty = true;
 }
 
 void ScoreEntity::addScoreAsteroidHit() {
     this->score += 50;
+    isDirty = true;
+}
+
+void ScoreEntity::render(SDL_Renderer& renderer) {
+    getComponent<TextComponent>()->render(renderer);
 }
 
 void ScoreEntity::reset() {
     this->score = 0;
-    getComponent<TextComponent>()->setText(toString());
+    isDirty = true;
+}
+
+void ScoreEntity::update(float deltaTime) {
+    if(isDirty) {
+        isDirty = false;
+        getComponent<TextComponent>()->setText(toString());
+    }
 }
 
 void ScoreEntity::update(const SDL_Event& event) {
     if(event.type == SDL_USEREVENT) {
         switch(event.user.code) {
             case ManagerHelper::EVENT_ASTEROID_HIT: {
-                
+                addScoreAsteroidHit();
                 break;
             }
         }
@@ -41,7 +53,4 @@ std::string ScoreEntity::toString() const {
     std::stringstream ss;
     ss << std::setw(precision) << std::setfill('0') << score;
     return ss.str();
-}
-void ScoreEntity::render(SDL_Renderer& renderer) {
-    getComponent<TextComponent>()->render(renderer);
 }
--- Asteroids/Game/Entities/ScoreEntity.hpp
+++ Asteroids/Game/Entities/ScoreEntity.hpp
@@ -11,9 +11,11 @@ public:
     void addScoreAsteroidHit();
     void reset();
     virtual void render(SDL_Renderer& renderer) override;
+    virtual void update(float deltaTime) override;
     virtual void update(const SDL_Event& event) override;
     std::string toString() const;
 private:
     int score = 0;
     const int precision = 6;
+    bool isDirty = true;
 };
