5ff48c77b9496507e993c2113e06f9df73dcbdbf
Daniel Ricci <thedanny09@icloud.com>
2021-03-03 22:17:07 -0500

    When rendering a transform component it will now display the coordinate axis (XY) w.r.t the orientation of the entity in its local space

--- Asteroids/Engine/Components/TransformComponent.hpp
+++ Asteroids/Engine/Components/TransformComponent.hpp
@@ -24,19 +24,18 @@ public:
     }
     
     virtual void render(SDL_Renderer& renderer) override {
-        //todo - fixme
-//        SDL_Color color;
-//        SDL_GetRenderDrawColor(&renderer, &color.r, &color.g, &color.b, &color.a);
-//
-//        Eigen::Vector2f position = getPosition();
-//        Eigen::Vector2f xPoint2 = ownerEntity->getPosition({30, 0});
-//        SDL_SetRenderDrawColor(&renderer, 0xFF, 0x00, 0x00, SDL_ALPHA_OPAQUE);
-//        SDL_RenderDrawLine(&renderer, position.x(), position.y(), xPoint2.x(), xPoint2.y());
-//        Eigen::Vector2f yPoint2 = ownerEntity->getPosition({0, 30});
-//        SDL_SetRenderDrawColor(&renderer, 0x00, 0xFF, 0x00, SDL_ALPHA_OPAQUE);
-//        SDL_RenderDrawLine(&renderer, position.x(), position.y(), yPoint2.x(), yPoint2.y());
-//
-//        SDL_SetRenderDrawColor(&renderer, color.r, color.g, color.b, color.a);
+        SDL_Color color;
+        SDL_GetRenderDrawColor(&renderer, &color.r, &color.g, &color.b, &color.a);
+
+        Eigen::Vector2f position = ownerEntity->getPosition();
+        Eigen::Vector2f xPoint2 = ownerEntity->getPosition({30, 0});
+        SDL_SetRenderDrawColor(&renderer, 0xFF, 0x00, 0x00, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(&renderer, position.x(), position.y(), xPoint2.x(), xPoint2.y());
+        Eigen::Vector2f yPoint2 = ownerEntity->getPosition({0, 30});
+        SDL_SetRenderDrawColor(&renderer, 0x00, 0xFF, 0x00, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(&renderer, position.x(), position.y(), yPoint2.x(), yPoint2.y());
+
+        SDL_SetRenderDrawColor(&renderer, color.r, color.g, color.b, color.a);
     }
        
     static const int ROTATION_RIGHT = 0;
